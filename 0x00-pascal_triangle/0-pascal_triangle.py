8fe3e2fc1066390452d5916587a333b76d87c2cf
malhaouit <abdelmounaim.abdel@gmail.com>
2024-06-28 00:11:22 +0000

    Update: remove the main method

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -17,22 +17,15 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
+
     triangle = []
-    
+
     for i in range(n):
-        # Start each row with 1
         row = [1] * (i + 1)
-        
-        # Calculate the inner elements of the row
+
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        
-        # Append the row to the triangle
-        triangle.append(row)
-    
-    return triangle
 
-if __name__ == "__main__":
-    from 0-main import print_triangle
-    print_triangle(pascal_triangle(5))
+        triangle.append(row)
+
+    return triangle
