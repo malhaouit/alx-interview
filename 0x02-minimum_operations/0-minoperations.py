29a0b15a256957d73d99d7b38aabdc699e2049ef
malhaouit <abdelmounaim.abdel@gmail.com>
2024-07-11 22:17:12 +0000

    Update: change the shebang

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 This module provides a function to calculate the minimum number of operations
 required to achieve exactly `n` 'H' characters in a text file using only
